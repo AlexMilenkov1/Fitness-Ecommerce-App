978056a0ccd9c28603ee405953b435e82df4f076
Alex Milenkov <alexmilenkov07@gmail.com>
2024-11-06 11:02:00 +0200

    Updated project

--- fitnessTracker/fitnessTracker/meals/choices.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class MealTypeChoices(models.TextChoices):
-    BREAKFAST = 'breakfast', 'breakfast'
-    LUNCH = 'lunch', 'lunch'
-    DINNER = 'dinner', 'dinner'
-    SNACK = 'snack', 'snack'
--- fitnessTracker/fitnessTracker/workout/choices.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ExerciseTypes(models.TextChoices):
-    CARDIO = 'cardio', 'cardio'
-    STRENGTH = 'strength', 'strength'
-    FLEXIBILITY = 'flexibility', 'flexibility'
-    BALANCE = 'balance', 'balance'
-    ENDURANCE = 'endurance', 'endurance'
