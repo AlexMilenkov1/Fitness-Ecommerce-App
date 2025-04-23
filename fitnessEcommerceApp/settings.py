bc23901d01a9039e96f80ef82df0277176a06df8
Alex Milenkov <alexmilenkov07@gmail.com>
2025-04-23 21:42:52 +0300

    added dockerfile, docker-compose.yml and configure the .env file

--- fitnessEcommerceApp/settings.py
+++ fitnessEcommerceApp/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'fitnessEcommerceApp.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "fitness_app_db",
-        "USER": "postgres",
-        "PASSWORD": "999999991",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
