434f0d8ebe8ed094eeab7f930d4f3b44d2e38909
Alex Milenkov <alexmilenkov07@gmail.com>
2025-04-27 13:52:29 +0300

    added images and .gitignore

--- fitnessEcommerceApp/settings.py
+++ fitnessEcommerceApp/settings.py
@@ -131,13 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
-)
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
